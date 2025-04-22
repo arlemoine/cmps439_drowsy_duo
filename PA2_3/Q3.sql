ef81b8a0b3bd7516ccfa2a6c86740210a5576544
Adriean Lemoine <adrieanlemoine@gmail.com>
2025-04-22 15:04:02 -0500

    Add PA2.3 directory

---
+++ PA2_3/Q3.sql
@@ -0,0 +1,54 @@
+/*
+TEAM NAME:
+TEAM MEMBERS' NAME:
+
+
+Instructions
+- Descriptions must reflect a business operation's need
+- One query for each item (Q..) is enough. E.g., for QD1: CREATE TABLE, write a DDL query to create one of your project's tables. Similar for the others.
+- You must use the exact format
+- Project a few attributes only unless otherwise said
+- Do not change the order of the queries
+*/
+
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ DML QUERIES   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+--QM9.1: INNER-JOIN-QUERY WITH WHERE CLAUSE:
+----Description: .....................
+
+
+--QM9.2: LEFT-OUTER-JOIN-QUERY WITH WHERE CLAUSE:
+----Instruction: The query must return NULL DUE TO MISMATCHING TUPLES during the outer join:
+----Description: .....................
+
+
+--QM9.3: RIGHT-OUTER-JOIN-QUERY WITH WHERE CLAUSE:
+----Instruction: The query must return NULL DUE TO MISMATCHING TUPLES during the outer join:
+----Description: .....................
+
+
+--QM9.4: FULL-OUTER-JOIN-QUERY WITH WHERE CLAUSE:
+----Instruction: The query must return NULL DUE TO MISMATCHING TUPLES from LEFT and RIGHT tables due to the outer join:
+----Description: .....................
+
+
+--QM10.1: AGGREGATION-JOIN-QUERY WITH GROUP BY & HAVING:
+----Description: .....................
+
+SELECT ...
+FROM .... JOIN ....
+WHERE ...
+GROUP BY ....
+HAVING .....
+
+--QM10.2: AGGREGATION-JOIN-QUERY WITH SUB-QUERY:
+----Description: .....................
+
+
+
+--QM11: WITH-QUERY:
+----Description: .....................
+
+
+
+
